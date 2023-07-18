25319b0e9916b0a887236465dedb890d31ca9d73
FlavioFCarvalho <flaviocfr@gmail.com>
2023-07-18 20:48:02 -0300

    first commit

---
+++ backend/src/main/java/com/flavio/posarquitetura/PosarquiteturaApplication.java
@@ -0,0 +1,13 @@
+package com.flavio.posarquitetura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosarquiteturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosarquiteturaApplication.class, args);
+	}
+
+}
